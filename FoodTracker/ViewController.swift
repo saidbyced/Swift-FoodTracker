68896feba6caf3178d99f78c07bc7c6c6dfb8644
Chris Eadie <chris.eadie@outlook.com>
2020-04-25 13:05:47 +0100

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Chris Eadie on 25/04/2020.
+//  Copyright © 2020 Chris Eadie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
