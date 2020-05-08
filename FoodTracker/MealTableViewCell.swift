e91013135127c4aebf3ccbdefa6cd2a124f0bd3c
Chris Eadie <chris.eadie@outlook.com>
2020-05-08 13:15:51 +0100

    Add second view - table view - and a cell controller for meals

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Chris Eadie on 08/05/2020.
+//  Copyright © 2020 Chris Eadie. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
