65c2c1121002090a786f3041c56c2eef9544d5fc
Chris Eadie <chris.eadie@outlook.com>
2020-05-11 10:49:22 +0100

    Save button saves update to existing meal

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -125,11 +125,17 @@ class MealTableViewController: UITableViewController {
     @IBAction func unwindtoMealList(sender:UIStoryboardSegue) {
         if let sourceViewController = sender.source as? MealViewController, let meal = sourceViewController.meal {
 
-            // Add a new meal.
-            let newIndexPath = IndexPath(row: meals.count, section: 0)
+            if let selectedIndexPath = tableView.indexPathForSelectedRow {
+                // Update an existing meal.
+                meals[selectedIndexPath.row] = meal
+                tableView.reloadRows(at: [selectedIndexPath], with: .none)
+            } else {
+                // Add a new meal.
+                let newIndexPath = IndexPath(row: meals.count, section: 0)
 
-            meals.append(meal)
-            tableView.insertRows(at: [newIndexPath], with: .automatic)
+                meals.append(meal)
+                tableView.insertRows(at: [newIndexPath], with: .automatic)
+            }
 
         }
     }
